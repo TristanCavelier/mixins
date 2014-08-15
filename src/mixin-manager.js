8943ca404791f3dda29534c6bb3187fb832acd8a
Tristan Cavelier <t.cavelier@free.fr>
2014-08-15 15:34:29 +0200

    update license and version of mixin-manager.js

--- src/mixin-manager.js
+++ src/mixin-manager.js
@@ -2,14 +2,35 @@
 (function factory(root) {
   "use strict";
 
-  // Version: 0.1.1
+  /*
+   Version: 0.1.2
 
-  // Copyright (c) 2014 Tristan Cavelier <t.cavelier@free.fr>
-  // This program is free software. It comes without any warranty, to
-  // the extent permitted by applicable law. You can redistribute it
-  // and/or modify it under the terms of the Do What The Fuck You Want
-  // To Public License, Version 2, as published by Sam Hocevar. See
-  // the COPYING file for more details.
+   Copyright (c) 2014 Tristan Cavelier <t.cavelier@free.fr>
+
+   This program is free software. It comes without any warranty, to
+   the extent permitted by applicable law. You can redistribute it
+   and/or modify it under the terms of the Do What The Fuck You Want
+   To Public License, Version 2, as published by Sam Hocevar. See
+   below for more details.
+
+   ___________________________________________________________________
+  |                                                                   |
+  |           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE             |
+  |                   Version 2, December 2004                        |
+  |                                                                   |
+  |Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>                   |
+  |                                                                   |
+  |Everyone is permitted to copy and distribute verbatim or modified  |
+  |copies of this license document, and changing it is allowed as long|
+  |as the name is changed.                                            |
+  |                                                                   |
+  |           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE             |
+  |  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION  |
+  |                                                                   |
+  | 0. You just DO WHAT THE FUCK YOU WANT TO.                         |
+  |___________________________________________________________________|
+
+  */
 
   /*jslint indent: 2, nomen: true, vars: true */
 
